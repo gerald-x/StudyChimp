4df24375099750fafa510a6efcda37e8685af301
gerald-x <geraldokolodc@gmail.com>
2023-09-10 00:10:20 +0100

    Created modeel for the books, and other materials being posted

--- interface/models.py
+++ interface/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from userAuthentication.models import User, Faculty
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=300, unique=True, null=False, blank=False)
+    contributor = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=False, related_name="author")
+    file = models.FileField(upload_to="documents/")
